94858591cbefdf508765dd064f24c4bea92baabf
s22h <frako@frako.dev>
2021-03-26 16:56:05 +0100

    Add a filter to not include draft posts

--- src/database.ts
+++ src/database.ts
@@ -11,6 +11,7 @@ const rootDir = path.resolve();
 export type GetPostOptions = {
 	limit?: number;
 	offset?: number;
+	includeDrafts?: boolean;
 };
 
 export async function getPost(slug: string): Promise<Post | null> {
@@ -40,42 +41,48 @@ async function getPostsFromFiles(files: string[], options: GetPostOptions) {
 	options.limit = options.limit || 10;
 	options.offset = options.offset || 0;
 
-	const posts = files.slice(options.offset, options.offset + options.limit).map(file => {
-		const fileData = fs.readFileSync(path.join(rootDir, "content/posts", file));
-		const frontmatter = matter(fileData);
+	let posts = files.slice(options.offset, options.offset + options.limit).map(
+		(file): Post => {
+			const fileData = fs.readFileSync(path.join(rootDir, "content/posts", file));
+			const frontmatter = matter(fileData);
 
-		const author = Authors.find(author => author.slug == frontmatter.data["author"]) || {
-			name: "Unknown author",
-			slug: "-"
-		};
-		const tags = (frontmatter.data["tags"] as string[]).map(slug => {
-			const tag = Tags.find(tag => tag.slug == slug);
-			if (tag) {
-				return {
-					...tag,
-					uri: `/tag/${tag.slug}`
-				};
-			}
-			return {
-				name: `${slug[0].toLocaleUpperCase()}${slug.substr(1)}`,
-				slug,
-				uri: `/tag/${slug}`
+			const author = Authors.find(author => author.slug == frontmatter.data["author"]) || {
+				name: "Unknown author",
+				slug: "-"
 			};
-		}, new Array<Tag>(frontmatter.data["tags"].length));
+			const tags = (frontmatter.data["tags"] as string[]).map(slug => {
+				const tag = Tags.find(tag => tag.slug == slug);
+				if (tag) {
+					return {
+						...tag,
+						uri: `/tag/${tag.slug}`
+					};
+				}
+				return {
+					name: `${slug[0].toLocaleUpperCase()}${slug.substr(1)}`,
+					slug,
+					uri: `/tag/${slug}`
+				};
+			}, new Array<Tag>(frontmatter.data["tags"].length));
 
-		return {
-			title: frontmatter.data["title"],
-			slug: frontmatter.data["slug"],
-			uri: `/post/${frontmatter.data["slug"]}`,
-			body: frontmatter.content,
-			pubDate: frontmatter.data["pubDate"],
-			author: {
-				...author,
-				uri: `/author/${author.slug}`
-			},
-			tags
-		};
-	});
+			return {
+				title: frontmatter.data["title"],
+				slug: frontmatter.data["slug"],
+				uri: `/post/${frontmatter.data["slug"]}`,
+				body: frontmatter.content,
+				pubDate: frontmatter.data["pubDate"],
+				author: {
+					...author,
+					uri: `/author/${author.slug}`
+				},
+				tags
+			};
+		}
+	);
+
+	if (!options.includeDrafts) {
+		posts = posts.filter(post => post.pubDate !== null);
+	}
 
 	return posts.sort((a, b) => {
 		const ad = new Date(a.pubDate);
